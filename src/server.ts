ea8176a290dea3f8432d37d524e3511d8626733b
manenim udoh <manenimabasi.udoh@stu.cu.edu.ng>
2022-09-27 11:35:38 +0100

    added filtered image

--- src/server.ts
+++ src/server.ts
@@ -1,4 +1,4 @@
-import express from 'express';
+import express, { Router, Request, Response } from 'express';
 import bodyParser from 'body-parser';
 import {filterImageFromURL, deleteLocalFiles} from './util/util';
 
@@ -28,6 +28,29 @@ import {filterImageFromURL, deleteLocalFiles} from './util/util';
   //   the filtered image file [!!TIP res.sendFile(filteredpath); might be useful]
 
   /**************************************************************************** */
+
+  // app.get('/filteredimage', async (req: Request, res: Response) => {
+  //   const { image_url } = req.query;
+  //   if (!image_url) {
+  //     return res.status(400).send({ message: 'image_url is required' });
+  //   }
+  //   const filteredpath = await filterImageFromURL(image_url);
+  //   res.sendFile(filteredpath, async () => {
+  //     await deleteLocalFiles([filteredpath]);
+  //   });
+  // })
+
+
+  app.get('/filteredimage', async (req: Request, res: Response) => {
+    const image_url  = req.query.image_url.toString();
+    if (!image_url) {
+      return res.status(400).send({ message: 'image_url is required' });
+    }
+    const filtered_image = await filterImageFromURL(image_url);
+    res.status(200).sendFile(filtered_image, async () => {
+      await deleteLocalFiles([filtered_image]);
+    });
+  })
 
   //! END @TODO1
   
